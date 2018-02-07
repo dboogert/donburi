44875857cd02be1293a671321fe5afef5ace5ce2
Don Boogert <donbo@image-engine.com>
2018-02-07 13:14:04 -0800

    write memory capture to disk, locking & few notes

--- MemFunctions.cpp
+++ MemFunctions.cpp
@@ -3,9 +3,14 @@
 
 #include <iostream>
 #include <iomanip>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #include <libunwind.h>
- 
+#include <pthread.h>
+
 typedef void* (*fn_malloc)(size_t);
 typedef void* (*fn_free)(void*);
 
@@ -23,12 +28,11 @@ struct Stack
         unw_cursor_t cursor;
         unw_init_local(&cursor, &context);
 
-        while( unw_step( &cursor ) && f < 16) //
+        while( unw_step( &cursor ) && f < 16)
         {
             unw_get_reg( &cursor, UNW_REG_IP, &ip[f] );
             f++;
         }
-
     }
 
     void print() const
@@ -66,39 +70,66 @@ struct MemOp
 
 struct MemOpBuffer
 {
-    MemOpBuffer() : index( 0 ) {}
+    MemOpBuffer() : index( 0 ), fd(-1)
+	{
+		const char* output = getenv("DONBURI_OUTPUTFILE");
+		if ( output )
+		{
+			strcpy(outputFile, output);
+
+			mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+			fd = open(outputFile, O_WRONLY | O_TRUNC | O_CREAT, mode);
+//			std::cout << fd << std::endl;
+		}
+	}
     ~MemOpBuffer()
     {
-        for (int i = 0; i < index; ++i)
-        {
-            const MemOp &op = ops[i];
+		flush();
+		close(fd);
 
-            if (op.malloc)
-            {
-                std::cout << "MALLOC: " <<  std::dec << op.size;
-                std::cout << " " << std::hex << std::setw(16) << std::setfill('0') <<  op.ptr;
-                std::cout << std::endl;
-
-            }
-            else
-            {
-                std::cout << "FREE ";
-                std::cout << " " << std::hex << std::setw(16) << std::setfill('0') <<  op.ptr;
-                std::cout << std::endl;
-            }
-            
-            op.stack.print();
-        }
+//        for (int i = 0; i < index; ++i)
+//        {
+//            const MemOp &op = ops[i];
+//
+//            if (op.malloc)
+//            {
+//                std::cout << "MALLOC: " <<  std::dec << op.size;
+//                std::cout << " " << std::hex << std::setw(16) << std::setfill('0') <<  op.ptr;
+//                std::cout << std::endl;
+//
+//            }
+//            else
+//            {
+//                std::cout << "FREE ";
+//                std::cout << " " << std::hex << std::setw(16) << std::setfill('0') <<  op.ptr;
+//                std::cout << std::endl;
+//            }
+//
+//            op.stack.print();
+//        }
     }
+
+	void flush()
+	{
+		write(fd, &ops[0], sizeof(MemOp) * index );
+		index = 0;
+	}
 
     void allocation(size_t size, void* ptr)
     {
+
         ops[index].stack.get();
         ops[index].size = size;
         ops[index].malloc = true;
         ops[index].ptr = ptr;
 
-        index++;
+		index++;
+
+		if (index == 1024 * 1024)
+		{
+			flush();
+		}
+
     }
 
     void free(void* ptr)
@@ -109,13 +140,29 @@ struct MemOpBuffer
         ops[index].ptr = ptr;
 
         index++;
+
+		if (index == 1024 * 1024)
+		{
+			flush();
+		}
     }
 
     int index;
     MemOp ops[1024 * 1024];
+	char outputFile[1024];
+	int fd;
 };
 
 MemOpBuffer gMemOpBuffer;
+pthread_mutex_t gMutex;
+
+// 1. lock access to MemOpBuffer [DONE]
+// 2. Serialise to profile file (environment variable for file)
+// 3. Insert marker API
+// 4. generate symbol from address only once
+// 5. guard against re-entry into malloc for a given thread so malloc & free work inside the instrumented malloc
+// 6. Write test case for threading
+// 7. cache address of old malloc & free
 
 extern "C"
 {
@@ -124,15 +171,18 @@ extern "C"
         fn_malloc o_malloc;
         o_malloc = (fn_malloc) dlsym(RTLD_NEXT, "malloc");
         void* ptr = o_malloc(size);
-        
-        gMemOpBuffer.allocation(size, ptr);
 
+		pthread_mutex_lock( &gMutex );
+        gMemOpBuffer.allocation(size, ptr);
+		pthread_mutex_unlock( &gMutex );
         return ptr;
     }
 
     void free(void *ptr)
     {
+		pthread_mutex_lock( &gMutex );
         gMemOpBuffer.free(ptr);
+		pthread_mutex_unlock( &gMutex );
 
         fn_free o_free;
         o_free = (fn_free) dlsym(RTLD_NEXT,"free");
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # donburi
+
 Memory profiler
+
+## Goals
+
+* Generate statistics on memory allocation with call site information.
+* Locate allocation / free patterns which might might benefit from a custom allocator.
+
+## Building
+
+### Platforms
+
+* Linux
+
+### Dependencies
+
+* cmake
+* imgui
+* libunwind
+
+## Usage
+
+## Implementation
+
+
+
